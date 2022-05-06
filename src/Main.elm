60aedc9241e74fd2f1a14abd41377468c5b7ecf4
gacallea <gacallea@users.noreply.github.com>
2022-05-06 17:04:45 +0200

    just saving some progress (about)

--- src/Main.elm
+++ src/Main.elm
@@ -7,6 +7,40 @@ import Html.Attributes exposing (..)
 
 
 
+-- CONSTANTS
+
+
+latestAlbumLink : String
+latestAlbumLink =
+    "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/363304199&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+
+
+soundCloudLink : String
+soundCloudLink =
+    "https://soundcloud.com/aascloud"
+
+
+spotifyLink : String
+spotifyLink =
+    "https://open.spotify.com/artist/00Cg2yZTaYr1EfRZDjStlh"
+
+
+appleMusicLink : String
+appleMusicLink =
+    "https://music.apple.com/us/artist/aner-andros/1034283469"
+
+
+youtubeLink : String
+youtubeLink =
+    "https://www.youtube.com/channel/UC2ZkNnT2pHj01jKuj56mJfg"
+
+
+deezerLink : String
+deezerLink =
+    "https://www.deezer.com/us/artist/8776748"
+
+
+
 -- MESSAGES
 
 
@@ -23,7 +57,6 @@ type alias Model =
         { width : Int
         , height : Int
         }
-    , hello : String
     }
 
 
@@ -47,7 +80,6 @@ init flags =
             { width = flags.win_width
             , height = flags.win_height
             }
-      , hello = "Hello Elm + Tailwind CSS :)"
       }
     , Cmd.none
     )
@@ -66,115 +98,192 @@ view _ =
         , hero
 
         -- , music
-        -- , about
+        , about
+
         -- , live
         , footer_
         ]
 
 
+
+-- HEADER
+
+
 header_ : Html Msg
 header_ =
-    header [ class "flex flex-row justify-center sm:justify-end" ] [ logo, navbar ]
-
-
-navlink : String -> String -> Html Msg
-navlink link name =
-    li
-        [ class "font-normal sm:font-medium" ]
-        [ a
-            [ class "p-2 hover:underline hover:underline-offset-4 hover:bg-slate-100", href <| link ]
-            [ text <| name ]
+    header
+        [ class "flex flex-row justify-center sm:justify-end sticky top-0 bg-white"
+        ]
+        [ logo_nav
+        , navbar
         ]
 
 
-logo : Html Msg
-logo =
-    div [ class "text-sm md:text-base my-2 ml-4 mr-auto hidden sm:inline-flex" ] [ text "aner andros" ]
+logo_nav : Html Msg
+logo_nav =
+    div
+        [ class "text-sm md:text-base my-2 ml-4 mr-auto hidden sm:inline-flex"
+        ]
+        [ text "aner andros"
+        ]
 
 
 navbar : Html Msg
 navbar =
     nav []
-        [ ul [ class "inline-flex text-sm md:text-base my-2 sm:mr-4" ]
+        [ ul
+            [ class "inline-flex text-sm md:text-base my-2 sm:mr-4"
+            ]
             [ navlink "#top" "home"
-            , navlink "#discog" "music"
+            , navlink "#music" "music"
             , navlink "#about" "about"
             , navlink "#live" "live"
             ]
         ]
 
 
+navlink : String -> String -> Html Msg
+navlink link name =
+    li
+        [ class "font-normal sm:font-medium"
+        ]
+        [ a
+            [ class "p-2 hover:underline hover:underline-offset-4 hover:bg-slate-100"
+            , href <| link
+            ]
+            [ text <| name
+            ]
+        ]
+
+
+
+-- HERO
+
+
 hero : Html Msg
 hero =
-    {-
-       hero (mobile: reduce/split. full: big aa head; h1; h2; // gwr logo + link
-                                    + featured/latest release with play option
-    -}
-    section [ id "hero", class "bg-purple-100 flex flex-row justify-evenly" ]
-        [ div [ class "align-top" ]
-            [ img [ src "./img/aner_andros-big.jpg", alt "aa logo" ] []
+    section
+        [ id "hero"
+        , class "flex flex-col sm:flex-row sm:justify-between sm:mx-4"
+        ]
+        [ div
+            [ class "text-xs lg:text-base text-center lg:text-justify"
             ]
-        , div [ class "align-middle" ]
-            [ h1 [ class "font-semibold text-5xl" ] [ text "aner andros" ]
-            , h2 [] [ text "musician. sound designer. creative coder. sound artist." ]
+            [ logo
+            , h1 [ class "sm:font-semibold py-4 overline" ]
+                [ text "musician. sound designer. creative coder. sound artist."
+                ]
             ]
         , div
-            [ class "align-bottom" ]
-            [ img [ src "./img/covers/aasb.jpg", alt "feat cover" ] []
-            , latestAlbum
+            [ class "text-sm lg:text-base text-center" ]
+            [ latestAlbum
+            , outlets
             ]
+        ]
+
+
+logo : Html Msg
+logo =
+    div
+        [ class "bg-gray-50 flex flex-row justify-center w-full"
+        ]
+        [ img [ src "https://placekitten.com/400/400", alt "aa logo" ] []
         ]
 
 
 latestAlbum : Html Msg
 latestAlbum =
-    p [ class "bg-slate-50" ] [ i [ class "bx-fw bx bx-play-circle" ] [], text "featured release w/ play option" ]
+    div
+        [ class "flex flex-col justify-center mb-2 mt-2 mx-2 border-t border-slate-300"
+        ]
+        [ h2 [ class "sm:font-semibold py-4 underline" ]
+            [ text "latest album"
+            ]
+        , iframe
+            [ class "w-full h-80"
+            , src latestAlbumLink
+            ]
+            []
+        ]
 
 
 socialIcon : String -> String -> Html msg
 socialIcon link icon =
     li
-        [ class "pl-0 px-5" ]
+        [ class "pl-0 px-5"
+        ]
         [ a
-            [ href <| link, target "_blank", rel "noopener noreferrer" ]
-            [ i [ class <| icon ] [] ]
+            [ href <| link
+            , target "_blank"
+            , rel "noopener noreferrer"
+            ]
+            [ i
+                [ class <| icon
+                ]
+                []
+            ]
         ]
 
 
 outlets : Html Msg
 outlets =
     div []
-        [ ul [ class "" ]
-            [ socialIcon "https://soundcloud.com/aascloud" "bx-fw bx bxl-soundcloud"
-            , socialIcon "https://open.spotify.com/artist/00Cg2yZTaYr1EfRZDjStlh" "bx-fw bx bxl-spotify"
-            , socialIcon "https://music.apple.com/us/artist/aner-andros/1034283469" "bx-fw bx bxl-apple"
-            , socialIcon "https://www.youtube.com/channel/UC2ZkNnT2pHj01jKuj56mJfg" "bx-fw bx bxl-youtube"
-            , socialIcon "https://www.deezer.com/us/artist/8776748" "bx-fw bx bxl-deezer"
+        [ ul
+            [ class "inline-flex py-2 justify-center"
+            ]
+            [ socialIcon soundCloudLink "bx-fw bx bxl-soundcloud"
+            , socialIcon spotifyLink "bx-fw bx bxl-spotify"
+            , socialIcon appleMusicLink "bx-fw bx bxl-apple"
+            , socialIcon youtubeLink "bx-fw bx bxl-youtube"
+            , socialIcon deezerLink "bx-fw bx bxl-deezer"
             ]
         ]
 
 
-discogCategory : String -> String -> Html Msg
-discogCategory anchor name =
-    li [ class "px-2 pr-0" ] [ a [ href <| anchor ] [ text <| name ] ]
+
+-- DISCOGRAPHY
+
+
+releaseKind : String -> String -> Html Msg
+releaseKind anchor name =
+    li
+        [ class "px-2 pr-0"
+        ]
+        [ a
+            [ href <| anchor
+            ]
+            [ text <| name ]
+        ]
 
 
 music : Html Msg
 music =
-    -- discog (mobile: 2x2 grid; full: horizontal scroll gallery)
-    section [ id "discog", class "bg-red-100 text-center" ]
-        [ nav [ class "bg-red-50" ]
-            [ ul [ class "flex flex-row justify-center" ]
-                [ discogCategory "#all" "all"
-                , discogCategory "#albums" "albums"
-                , discogCategory "#eps" "eps"
-                , discogCategory "#remixes" "remixes"
-                , discogCategory "#collages" "collages"
+    -- music (mobile: 2x2 grid; full: horizontal scroll gallery)
+    section
+        [ id "music"
+        , class "bg-red-100 text-center"
+        ]
+        [ nav
+            [ class "bg-red-50"
+            ]
+            [ ul
+                [ class "flex flex-row justify-center"
+                ]
+                [ releaseKind "#all" "all"
+                , releaseKind "#albums" "albums"
+                , releaseKind "#eps" "eps"
+                , releaseKind "#remixes" "remixes"
+                , releaseKind "#collages" "collages"
                 ]
             ]
-        , text "discography 'gallery' goes here"
-        , div [ class "bg-gray-50 flex flex-row justify-center py-2 text-sm" ]
-            [ p [] [ text "℗ © ", a [ href "https://gentlewashrecords.com/", target "_blank", rel "noopener noreferrer" ] [ text "Gentle Wash Records" ] ] ]
+        , discog
+        ]
+
+
+discog : Html Msg
+discog =
+    div []
+        [ text "dada"
         ]
 
 
@@ -183,11 +292,14 @@ about =
     -- about (resp. paragraph)
     section
         [ id "about"
-        , class "bg-emerald-200"
+        , class "flex flex-col text-sm lg:text-base mt-2 mx-2 border-t border-slate-300"
         ]
-        [ div [ class "px-4 py-2" ]
+        [ h2 [ class "sm:font-semibold pt-4 underline text-center" ]
+            [ text "about"
+            ]
+        , div [ class "p-4" ]
             [ p [ class "p-1" ]
-                [ text "Eclectic non-musician and producer of cinematic ambient, avant-garde, left-field, and softronica. Fond of improvisation, of the studio as an instrument and of meticulously prepared live shows. While preserving empathy and intuition as key factors when drawing oneiric soundscapes into a story, in the making of musical voyages. "
+                [ text "Eclectic producer of cinematic ambient, avant-garde, left-field, glitch, and softronica. Fond of improvisation, of the studio as an instrument and of meticulously prepared live shows. While preserving empathy and intuition as key factors when drawing oneiric soundscapes in the making of musical voyages. "
                 , a
                     [ href "https://vfs.edu/programs/sound-design"
                     , target "_blank"
@@ -195,7 +307,7 @@ about =
                     ]
                     [ text "VFS Alumnus and Sound Design for Visual Media graduate." ]
                 ]
-            , p [ class "p-1" ] [ text "Member of Mi.S.Fu and Sleep Collective: a multifunctional, versatile cross-media laboratory dedicated to mapping, music, 3D, arts; and a collective consisting of talents that had given rise to various Sleep Concert in different locations and with different line-ups." ]
+            , p [ class "p-1" ] [ text "Member of Mi.S.Fu and Sleep Collective. The former: a multifunctional, versatile cross-media laboratory dedicated to mapping, music, 3D, arts. The latter: a collective consisting of talents that had given rise to various Sleep Concert in different Italian locations and with different line-ups." ]
             , p [ class "p-1" ]
                 [ text "Formerly based in Bristol, UK, Aner Andros has performed at the "
                 , a
@@ -211,7 +323,7 @@ about =
                     , rel "noopener noreferrer"
                     ]
                     [ text "Parallel Madness" ]
-                , text " Also noteworthy are the live shows at "
+                , text ". Other noteworthy shows include "
                 , a
                     [ href "https://crux-events.org/"
                     , target "_blank"
@@ -232,7 +344,7 @@ live =
 
 footer_ : Html Msg
 footer_ =
-    footer [ class "flex flex-row justify-center text-xs md:text-sm mt-2 mx-10 md:mx-4 border-t border-slate-300" ]
+    footer [ class "flex flex-row justify-center text-xs md:text-sm mt-2 mx-2 border-t border-slate-300" ]
         [ div [ class "py-4 text-center" ]
             [ p []
                 [ text "© Copyright Aner Andros. All Rights Reserved." ]
