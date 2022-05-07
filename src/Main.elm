3825fbe530524ec72f05aba3e9f9790880fb9d66
gacallea <gacallea@users.noreply.github.com>
2022-05-07 11:35:04 +0200

    added ARIA (just started) + minior changes

--- src/Main.elm
+++ src/Main.elm
@@ -1,13 +1,19 @@
 module Main exposing (Flags, Model, Msg(..), main)
 
+import Accessibility as Html exposing (..)
+import Accessibility.Aria as Aria exposing (..)
 import Browser
 import Browser.Events
-import Html exposing (..)
 import Html.Attributes exposing (..)
 
 
 
 -- CONSTANTS
+
+
+latestAlbumTitle : String
+latestAlbumTitle =
+    "Artefacts and Sonic Brushstrokes"
 
 
 latestAlbumLink : String
@@ -93,13 +99,12 @@ view : Model -> Html Msg
 view _ =
     main_
         [ class "container mx-auto max-w-7xl"
+        , Aria.label "main content"
         ]
         [ header_
         , hero
         , music
         , about
-
-        -- , live
         , footer_
         ]
 
@@ -111,30 +116,37 @@ view _ =
 header_ : Html Msg
 header_ =
     header
-        [ class "flex flex-row justify-center sm:justify-end sticky top-0 bg-white"
+        [ class "flex flex-row justify-center sm:justify-between sticky top-0 bg-white md:mb-4 md:py-2 border-0 md:border-b border-slate-300"
+        , Aria.label "main header"
         ]
-        [ logo_nav
+        [ logo
         , navbar
         ]
 
 
-logo_nav : Html Msg
-logo_nav =
+logo : Html Msg
+logo =
     div
-        [ class "text-md md:text-base my-2 sm:ml-4 mr-auto hidden sm:inline-flex font-normal sm:font-medium"
+        [ class "text-md md:text-base my-2 sm:ml-4 xl:ml-0 hidden sm:inline-flex font-normal sm:font-medium"
+        , Aria.label "brand name"
         ]
         [ ul
             [ class "inline-flex text-md md:text-base"
             ]
-            [ navLink "#top" "aner andros" ]
+            [ li
+                [ class "pl-2"
+                ]
+                [ text "aner andros"
+                ]
+            ]
         ]
 
 
 navbar : Html Msg
 navbar =
-    nav []
+    nav [ Aria.label "main menu" ]
         [ ul
-            [ class "inline-flex text-md md:text-base my-2 sm:mr-4"
+            [ class "inline-flex text-md md:text-base my-2 sm:mr-4 xl:mr-0"
             ]
             [ navLink "#top" "home"
             , navLink "#music" "music"
@@ -147,7 +159,7 @@ navbar =
 navLink : String -> String -> Html Msg
 navLink link name =
     li
-        []
+        [ Aria.label "main menu link" ]
         [ a
             [ class "p-2 hover:underline hover:underline-offset-4 hover:bg-slate-100"
             , href <| link
@@ -160,7 +172,7 @@ navLink link name =
 extLink : String -> String -> Html Msg
 extLink link name =
     li
-        []
+        [ Aria.label "main menu link" ]
         [ a
             [ class "p-2 hover:underline hover:underline-offset-4 hover:bg-slate-100"
             , href <| link
@@ -179,31 +191,38 @@ extLink link name =
 hero : Html Msg
 hero =
     section
-        [ id "hero"
-        , class "flex flex-col sm:flex-row sm:justify-around sm:mx-2 lg:m-8"
-        ]
+        [ id "hero", class "sm:mx-2 lg:m-8", Aria.label "hero section" ]
         [ div
-            [ class "text-sm lg:text-base text-center lg:text-justify"
+            [ class "flex flex-col sm:flex-row sm:justify-around mb-2 mt-0 md:mt-2 mx-2 border-t md:border-0 border-slate-300 text-center"
             ]
-            [ logo
-            , h1 [ class "sm:font-semibold py-4 overline" ]
-                [ text "musician. sound designer. creative coder. sound artist."
+            [ h2
+                [ class "sm:font-bold py-2 block underline md:hidden landscape:hidden" ]
+                [ text "aner andros"
+                ]
+            , div
+                [ class "text-sm lg:text-base lg:text-justify"
+                ]
+                [ brand
+                , h1 [ class "sm:font-semibold py-4 overline" ]
+                    [ text "musician. sound designer. creative coder. sound artist."
+                    ]
+                ]
+            , div
+                [ class "text-md lg:text-base content-end" ]
+                [ latestAlbum
+                , outlets
                 ]
             ]
-        , div
-            [ class "text-md lg:text-base text-center content-end" ]
-            [ latestAlbum
-            , outlets
-            ]
         ]
 
 
-logo : Html Msg
-logo =
+brand : Html Msg
+brand =
     div
         [ class "flex flex-row justify-center w-full mt-0 md:mt-2"
+        , Aria.label "brand logo"
         ]
-        [ img [ src "https://placekitten.com/400/400", alt "aa logo" ] []
+        [ img "Aner Andros Logo" [ src "https://placekitten.com/400/400", alt "Aner Andros" ]
         ]
 
 
@@ -211,6 +230,7 @@ latestAlbum : Html Msg
 latestAlbum =
     div
         [ class "flex flex-col justify-center mb-2 mt-0 md:mt-2 mx-2 border-t md:border-0 border-slate-300"
+        , Aria.label "latest album player"
         ]
         [ h2 [ class "sm:font-semibold py-4 md:p-0 underline block md:hidden landscape:hidden" ]
             [ text "latest release"
@@ -218,15 +238,17 @@ latestAlbum =
         , iframe
             [ class "w-full h-80"
             , src latestAlbumLink
+            , title latestAlbumTitle
             ]
             []
         ]
 
 
-socialIcon : String -> String -> Html msg
-socialIcon link icon =
+socialIcon : String -> String -> String -> Html msg
+socialIcon link icon socialname =
     li
         [ class "pl-0 px-5"
+        , Aria.label socialname
         ]
         [ a
             [ href <| link
@@ -243,15 +265,15 @@ socialIcon link icon =
 
 outlets : Html Msg
 outlets =
-    div []
+    div [ Aria.label "music outlets social links" ]
         [ ul
             [ class "inline-flex py-2 justify-center text-lg lg:text-xl"
             ]
-            [ socialIcon soundCloudLink "bx-fw bx bxl-soundcloud"
-            , socialIcon spotifyLink "bx-fw bx bxl-spotify"
-            , socialIcon appleMusicLink "bx-fw bx bxl-apple"
-            , socialIcon youtubeLink "bx-fw bx bxl-youtube"
-            , socialIcon deezerLink "bx-fw bx bxl-deezer"
+            [ socialIcon soundCloudLink "bx-fw bx bxl-soundcloud" "soundcloud"
+            , socialIcon spotifyLink "bx-fw bx bxl-spotify" "spotify"
+            , socialIcon appleMusicLink "bx-fw bx bxl-apple" "apple"
+            , socialIcon youtubeLink "bx-fw bx bxl-youtube" "youtube"
+            , socialIcon deezerLink "bx-fw bx bxl-deezer" "deezer"
             ]
         ]
 
@@ -262,18 +284,15 @@ outlets =
 
 music : Html Msg
 music =
-    -- music (mobile: 2x2 grid; full: horizontal scroll gallery)
     section
-        [ id "music"
-        , class "flex flex-col text-md lg:text-base mt-2 mx-1 md:mx-2 border-t border-slate-300"
-        ]
-        [ h2 [ class "sm:font-semibold pt-4 mb-4 underline text-center" ]
-            [ text "music"
-            ]
-        , discog
-        , div
-            []
-            [ ul
+        [ id "music", class "mt-2 mx-1 md:mx-2", Aria.label "discography section" ]
+        [ div
+            [ class "flex flex-col text-md lg:text-base border-t border-slate-300 text-center" ]
+            [ h2 [ class "sm:font-semibold pt-4 mb-4 underline" ]
+                [ text "music"
+                ]
+            , discog
+            , ul
                 [ class "flex flex-row justify-center text-sm lg:text-base my-4 sm:mr-4"
                 ]
                 [ releaseKind "#all" "all"
@@ -289,7 +308,8 @@ music =
 releaseKind : String -> String -> Html Msg
 releaseKind anchor name =
     li
-        [ class "px-2 pr-0 hover:underline hover:underline-offset-4"
+        [ class "px-2 hover:underline hover:underline-offset-4"
+        , Aria.label "kind of release selectors"
         ]
         [ a
             [ href <| anchor
@@ -300,8 +320,25 @@ releaseKind anchor name =
 
 discog : Html Msg
 discog =
-    div [ class "bg-pink-50 py-8 text-center" ]
-        [ text "dada"
+    ul
+        [ class "flex flex-row justify-between p-2 m-2"
+        , Aria.label "all published releases"
+        ]
+        [ li [] [ release "https://placekitten.com/300" "Artefacts and Sonic Brushstrokes" "ep" ]
+        , li [] [ release "https://placekitten.com/300" "Liminal Tales" "album" ]
+        , li [] [ release "https://placekitten.com/300" "Embroidering on Silence Vol. I" "ep" ]
+        , li [] [ release "https://placekitten.com/300" "Remap Ethos Stream" "album" ]
+        , li [] [ release "https://placekitten.com/300" "Enkefalina (Aner Andros Remix)" "remix" ]
+        , li [] [ release "https://placekitten.com/300" "ISPIRAZIONE" "collage" ]
+        ]
+
+
+release : String -> String -> String -> Html Msg
+release cover name kind =
+    img name
+        [ src cover
+        , alt name
+        , title name
         ]
 
 
@@ -313,46 +350,46 @@ about : Html Msg
 about =
     -- about (resp. paragraph)
     section
-        [ id "about"
-        , class "flex flex-col text-md lg:text-base mt-2 mx-1 md:mx-2 border-t border-slate-300"
-        ]
-        [ h2 [ class "sm:font-semibold pt-4 mb-4 underline text-center" ]
-            [ text "about"
-            ]
-        , div [ class "bg-gray-50 p-8 mb-4 md:mb-8" ]
-            [ p [ class "p-1" ]
-                [ text "Eclectic producer of cinematic ambient, avant-garde, left-field, glitch, and softronica. Fond of improvisation, of the studio as an instrument and of meticulously prepared live shows. While preserving empathy and intuition as key factors when drawing oneiric soundscapes in the making of musical voyages. "
-                , a
-                    [ href "https://vfs.edu/programs/sound-design"
-                    , target "_blank"
-                    , rel "noopener noreferrer"
-                    ]
-                    [ text "VFS Alumnus and Sound Design for Visual Media graduate." ]
+        [ id "about", class "mt-2 mx-1 md:mx-2", Aria.label "about section" ]
+        [ div [ class "flex flex-col text-md lg:text-base border-t border-slate-300" ]
+            [ h2 [ class "sm:font-semibold pt-4 mb-4 underline text-center" ]
+                [ text "about"
                 ]
-            , p [ class "p-1" ] [ text "Member of Mi.S.Fu and Sleep Collective. The former: a multifunctional, versatile cross-media laboratory dedicated to mapping, music, 3D, arts. The latter: a collective consisting of talents that had given rise to various Sleep Concert in different Italian locations and with different line-ups." ]
-            , p [ class "p-1" ]
-                [ text "Formerly based in Bristol, UK, Aner Andros has performed at the "
-                , a
-                    [ href "https://cubecinema.com/"
-                    , target "_blank"
-                    , rel "noopener noreferrer"
+            , div [ class "bg-gray-50 p-8 mb-4 md:mb-8" ]
+                [ p [ class "p-1" ]
+                    [ text "Eclectic producer of cinematic ambient, avant-garde, left-field, glitch, and softronica. Fond of improvisation, of the studio as an instrument and of meticulously prepared live shows. While preserving empathy and intuition as key factors when drawing oneiric soundscapes in the making of musical voyages. "
+                    , a
+                        [ href "https://vfs.edu/programs/sound-design"
+                        , target "_blank"
+                        , rel "noopener noreferrer"
+                        ]
+                        [ text "VFS Alumnus and Sound Design for Visual Media graduate." ]
                     ]
-                    [ text "Cube Cinema Plex" ]
-                , text " and has collaborated with "
-                , a
-                    [ href "https://parallelmadness.com/"
-                    , target "_blank"
-                    , rel "noopener noreferrer"
+                , p [ class "p-1" ] [ text "Member of Mi.S.Fu and Sleep Collective. The former: a multifunctional, versatile cross-media laboratory dedicated to mapping, music, 3D, arts. The latter: a collective consisting of talents that had given rise to various Sleep Concert in different Italian locations and with different line-ups." ]
+                , p [ class "p-1" ]
+                    [ text "Formerly based in Bristol, UK, Aner Andros has performed at the "
+                    , a
+                        [ href "https://cubecinema.com/"
+                        , target "_blank"
+                        , rel "noopener noreferrer"
+                        ]
+                        [ text "Cube Cinema Plex" ]
+                    , text " and has collaborated with "
+                    , a
+                        [ href "https://parallelmadness.com/"
+                        , target "_blank"
+                        , rel "noopener noreferrer"
+                        ]
+                        [ text "Parallel Madness" ]
+                    , text ". Other noteworthy shows include "
+                    , a
+                        [ href "https://crux-events.org/"
+                        , target "_blank"
+                        , rel "noopener noreferrer"
+                        ]
+                        [ text "Crux Events in London" ]
+                    , text " and the Cronosfera Festival in Italy."
                     ]
-                    [ text "Parallel Madness" ]
-                , text ". Other noteworthy shows include "
-                , a
-                    [ href "https://crux-events.org/"
-                    , target "_blank"
-                    , rel "noopener noreferrer"
-                    ]
-                    [ text "Crux Events in London" ]
-                , text " and the Cronosfera Festival in Italy."
                 ]
             ]
         ]
@@ -364,15 +401,15 @@ about =
    live : Html Msg
    live =
        -- live (resp. sort of calendar)
-       section [ id "live", class "bg-pink-200" ] [ text "live" ]
+       section [ id "live", Aria.label "live shows section" ] [ text "live" ]
 -}
 -- FOOTER
 
 
 footer_ : Html Msg
 footer_ =
-    footer [ class "flex flex-row justify-center text-sm md:text-sm mt-2 mx-2 border-t border-slate-300" ]
-        [ div [ class "py-4 text-center" ]
+    footer [ id "footer", class "mt-2", Aria.label "footer" ]
+        [ div [ class "flex flex-col justify-center text-sm md:text-sm border-t border-slate-300 py-4 text-center" ]
             [ p []
                 [ text "© Copyright Aner Andros. All Rights Reserved." ]
             , p []
