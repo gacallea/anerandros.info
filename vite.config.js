57e585cffa20a532a69b070b207900e8f9152010
gacallea <gacallea@users.noreply.github.com>
2022-05-10 15:54:25 +0200

    vire conf + favicons dynamic

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,28 @@
 import { defineConfig } from 'vite'
 import elmPlugin from 'vite-plugin-elm'
+import { ViteFaviconsPlugin } from 'vite-plugin-favicon'
+import compress from 'vite-plugin-compress'
 
 export default defineConfig({
-  plugins: [elmPlugin()]
+  plugins: [
+    elmPlugin(),
+    compress(),
+    ViteFaviconsPlugin({
+      logo: './src/img/aner_andros-big.jpg', // svg works too!
+      favicons: {
+        appName: 'aner_andros',
+        appDescription: 'Aner Andros home page',
+        developerName: 'Andrea Callea',
+        developerURL: null, // prevent retrieving from the nearest package.json
+        icons: {
+          coast: false,
+          yandex: false,
+          windows: false
+        }
+      }
+    })
+  ],
+  build: {
+    outDir: 'public'
+  }
 })
