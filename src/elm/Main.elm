3e543a39a36764fb53b03aa19cd3989c3282cfae
gacallea <gacallea@users.noreply.github.com>
2022-05-21 09:51:45 +0200

    whatever

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -207,14 +207,13 @@ hero =
             [ Attr.class "mx-0 md:mx-14 flex flex-col sm:justify-between text-center"
             ]
             [ div
-                [ Attr.class "text-sm lg:text-base bg-emerald-100"
+                [ Attr.class "text-sm lg:text-base"
                 ]
                 [ brand
                 , h1 [ Attr.class "sm:font-semibold py-4 overline" ]
                     [ Html.text "musician. sound designer. creative coder. sound artist."
                     ]
                 ]
-            , latestAlbumPlayer
             ]
         ]
 
@@ -226,7 +225,7 @@ brand =
         , Aria.label "brand logo"
         ]
         [ img
-            [ Attr.class "aspect-square w-full md:w-1/2 h-auto mt-2 md:mt-4 sm:rounded-full border-slate-300 sm:border border-dashed"
+            [ Attr.class "aspect-square w-full md:w-1/2 h-auto mt-2 md:mt-4 rounded-full border-slate-300 border border-dashed"
             , Attr.src anerandros.logo
             , Attr.alt "Aner Andros"
             ]
@@ -234,112 +233,104 @@ brand =
         ]
 
 
-latestAlbumPlayer : Html Msg
-latestAlbumPlayer =
+
+-- DISCOGRAPHY
+
+
+music : Model -> Html Msg
+music model =
+    section
+        [ Attr.id "music", Attr.class "mx-0 md:mx-2", Aria.label "discography section" ]
+        [ div
+            [ Attr.class "text-md lg:text-base border-t border-slate-300 text-center md:px-14" ]
+            [ h2 [ Attr.class "font-semibold pt-4 mb-4 md:mb-6 underline" ]
+                [ Html.text "music"
+                ]
+            , discog model
+            , ul
+                [ Attr.class "flex flex-row justify-center mt-2 py-2"
+                ]
+              <|
+                releaseSelector selectors
+            , latestAlbumPlayer
+            ]
+        ]
+
+
+selectors : List ( String, ReleaseKind )
+selectors =
+    [ ( "all", All )
+    , ( "albums", Albums )
+    , ( "eps", Eps )
+    , ( "remixes", Remixes )
+    , ( "collages", Collages )
+    ]
+
+
+releaseSelector : List ( String, ReleaseKind ) -> List (Html Msg)
+releaseSelector list =
+    List.map
+        (\sel ->
+            li
+                [ Attr.class "m-2 mt-0 hover:underline hover:underline-offset-4"
+                , Aria.label "kind of release selectors"
+                , onClick (ChooseRelease (Tuple.second sel))
+                ]
+                [ a
+                    [ Attr.href <| String.append "#" (Tuple.first sel)
+                    ]
+                    [ Html.text <| Tuple.first sel ]
+                ]
+        )
+        list
+
+
+discog : Model -> Html Msg
+discog model =
     div
-        [ Attr.class "flex flex-col justify-center border-t sm:border-0 border-slate-300 pb-8 pt-0 md:pt-8 sm:self-end text-base content-end md:w-full bg-amber-100"
-        , Aria.label "latest album player"
+        [ Attr.class "grid grid-flow-col auto-cols-max gap-2 overflow-contain overflow-x-scroll overflow-y-hidden snap-mandatory snap-x"
+        , Aria.label "all published releases"
         ]
-        [ h2 [ Attr.class "sm:font-semibold py-4 md:p-0 underline block md:hidden landscape:hidden" ]
-            [ Html.text "latest release"
-            ]
-        , div
-            [ Attr.class "mx-8 md:mx-0 h-auto text-left max-w-md md:max-w-full bg-white rounded-xl md:rounded-none shadow-md overflow-hidden"
-            ]
-            [ div
-                [ Attr.class "md:flex"
-                ]
-                [ div
-                    [ Attr.class "md:shrink-0"
+    <|
+        renderReleases <|
+            case model.showRelease of
+                All ->
+                    showReleases allReleases
+
+                Albums ->
+                    showReleases allAlbums
+
+                Eps ->
+                    showReleases allEps
+
+                Remixes ->
+                    showReleases allRemixes
+
+                Collages ->
+                    showReleases allCollages
+
+
+renderReleases : List Release -> List (Html msg)
+renderReleases data =
+    List.map
+        (\i ->
+            div
+                [ Attr.class "snap-always snap-center min-w-fit object-cover cursor-pointer group hover:bg-slate-900" ]
+                [ img
+                    [ --Attr.class "aspect-square w-full h-auto max-w-xs lg:max-w-md group-hover:opacity-70"
+                      Attr.class "w-64 h-64 md:w-72 md:h-72 max-w-xs lg:max-w-md group-hover:opacity-70"
+                    , Attr.src <| i.cover
+                    , Attr.alt i.name
+                    , Attr.title i.name
                     ]
-                    [ img
-                        [ Attr.class "h-56 w-full object-cover md:h-52 md:w-52"
-                        , Attr.src latestAlbum.cover
-                        , Attr.alt latestAlbum.name
-                        , Attr.title latestAlbum.name
-                        ]
-                        []
-                    ]
+                    []
                 , div
-                    [ Attr.class "w-4/5 m-auto py-4 md:py-2 md:px-8"
+                    [ Attr.class "absolute top-1/2 w-full invisible group-hover:visible text-center group-hover:text-white group-hover:text-sm group-hover:lg:text-md font-light"
                     ]
-                    [ h1
-                        [ Attr.class "block text-lg md:text-xl leading-tight font-medium text-gray-900"
-                        ]
-                        [ Html.text "00h" ]
-                    , p
-                        [ Attr.class "text-sm md:text-base text-gray-700 my-2"
-                        ]
-                        [ Html.text latestAlbum.name ]
-                    , div []
-                        [ div
-                            [ Attr.class "flex justify-between items-center text-gray-700 text-xl mt-6 md:mt-4 w-5/6 md:max-w-xs mx-auto"
-                            ]
-                            [ div
-                                []
-                                [ svg
-                                    [ SvgAttr.class "w-4 h-4"
-                                    , SvgAttr.fill "currentColor"
-                                    , SvgAttr.viewBox "0 0 20 20"
-                                    ]
-                                    [ Svg.path
-                                        [ SvgAttr.d "M6.59 12.83L4.4 15c-.58.58-1.59 1-2.4 1H0v-2h2c.29 0 .8-.2 1-.41l2.17-2.18 1.42 1.42zM16 4V1l4 4-4 4V6h-2c-.29 0-.8.2-1 .41l-2.17 2.18L9.4 7.17 11.6 5c.58-.58 1.59-1 2.41-1h2zm0 10v-3l4 4-4 4v-3h-2c-.82 0-1.83-.42-2.41-1l-8.6-8.59C2.8 6.21 2.3 6 2 6H0V4h2c.82 0 1.83.42 2.41 1l8.6 8.59c.2.2.7.41.99.41h2z"
-                                        ]
-                                        []
-                                    ]
-                                ]
-                            , div
-                                []
-                                [ Icon.viewIcon Icon.backward
-                                ]
-                            , div
-                                [ Attr.class "text-5xl text-red-500" ]
-                                [ --Icon.viewIcon Icon.playCircle
-                                  Icon.viewIcon Icon.pauseCircle
-                                ]
-                            , div
-                                []
-                                [ Icon.viewIcon Icon.forward
-                                ]
-                            , div
-                                []
-                                [ svg
-                                    [ SvgAttr.class "w-5 h-5"
-                                    , SvgAttr.fill "currentColor"
-                                    , SvgAttr.viewBox "0 0 20 20"
-                                    ]
-                                    [ Svg.path
-                                        [ SvgAttr.d "M5 4a2 2 0 0 0-2 2v6H0l4 4 4-4H5V6h7l2-2H5zm10 4h-3l4-4 4 4h-3v6a2 2 0 0 1-2 2H6l2-2h7V8z"
-                                        ]
-                                        []
-                                    ]
-                                ]
-                            ]
-                        , div
-                            [ Attr.class "flex justify-between text-sm text-gray-700"
-                            ]
-                            [ p []
-                                [ Html.text "0:42" ]
-                            , p []
-                                [ Html.text "4:20" ]
-                            ]
-                        , div
-                            []
-                            [ div
-                                [ Attr.class "h-1 bg-gray-800 rounded-full"
-                                ]
-                                [ div
-                                    [ Attr.class "w-1/5 h-1 bg-red-500 rounded-full relative"
-                                    ]
-                                    []
-                                ]
-                            ]
-                        , outlets latestAlbumOutlets "text-gray-700 text-lg md:text-base mt-4" "px-2"
-                        ]
-                    ]
+                    [ Html.text i.name ]
                 ]
-            ]
-        ]
+        )
+        data
 
 
 outlets : List ( String, String ) -> String -> String -> Html Msg
@@ -413,102 +404,112 @@ returnicon str =
             Icon.smile
 
 
-
--- DISCOGRAPHY
-
-
-music : Model -> Html Msg
-music model =
-    section
-        [ Attr.id "music", Attr.class "mx-0 md:mx-2", Aria.label "discography section" ]
-        [ div
-            [ Attr.class "text-md lg:text-base border-t border-slate-300 text-center md:px-14" ]
-            [ h2 [ Attr.class "sm:font-semibold pt-4 mb-4 md:mb-6 underline" ]
-                [ Html.text "music"
+latestAlbumPlayer : Html Msg
+latestAlbumPlayer =
+    div
+        [ Attr.class "flex flex-col justify-center border-t border-slate-300 pb-8 sm:self-end text-base content-end sm:w-full"
+        , Aria.label "latest album player"
+        ]
+        [ p [ Attr.class "font-semibold py-4 underline text-md" ]
+            [ Html.text "latest release"
+            ]
+        , div
+            [ Attr.class "mx-8 sm:mx-0 h-auto text-left max-w-md sm:max-w-full bg-white rounded-xl sm:rounded-none shadow-md overflow-hidden"
+            ]
+            [ div
+                [ Attr.class "sm:flex"
                 ]
-            , discog model
-            , ul
-                [ Attr.class "flex flex-row justify-center mt-2 py-2"
+                [ div
+                    [ Attr.class "sm:shrink-0"
+                    ]
+                    [ img
+                        [ Attr.class "h-56 w-full object-cover md:h-52 md:w-52"
+                        , Attr.src latestAlbum.cover
+                        , Attr.alt latestAlbum.name
+                        , Attr.title latestAlbum.name
+                        ]
+                        []
+                    ]
+                , div
+                    [ Attr.class "w-4/5 m-auto py-4 sm:py-2 sm:px-8"
+                    ]
+                    [ h1
+                        [ Attr.class "block text-lg sm:text-xl leading-tight font-medium text-gray-900"
+                        ]
+                        [ Html.text "00h" ]
+                    , p
+                        [ Attr.class "text-sm sm:text-base text-gray-700 my-2"
+                        ]
+                        [ Html.text latestAlbum.name ]
+                    , div []
+                        [ div
+                            [ Attr.class "flex justify-around items-center text-gray-700 text-xl mt-6 sm:mt-4 w-10/12 sm:w-7/12 lg:w-4/12 mx-auto"
+                            ]
+                            [ div
+                                []
+                                [ svg
+                                    [ SvgAttr.class "w-4 h-4"
+                                    , SvgAttr.fill "currentColor"
+                                    , SvgAttr.viewBox "0 0 20 20"
+                                    ]
+                                    [ Svg.path
+                                        [ SvgAttr.d "M6.59 12.83L4.4 15c-.58.58-1.59 1-2.4 1H0v-2h2c.29 0 .8-.2 1-.41l2.17-2.18 1.42 1.42zM16 4V1l4 4-4 4V6h-2c-.29 0-.8.2-1 .41l-2.17 2.18L9.4 7.17 11.6 5c.58-.58 1.59-1 2.41-1h2zm0 10v-3l4 4-4 4v-3h-2c-.82 0-1.83-.42-2.41-1l-8.6-8.59C2.8 6.21 2.3 6 2 6H0V4h2c.82 0 1.83.42 2.41 1l8.6 8.59c.2.2.7.41.99.41h2z"
+                                        ]
+                                        []
+                                    ]
+                                ]
+                            , div
+                                []
+                                [ Icon.viewIcon Icon.backward
+                                ]
+                            , div
+                                [ Attr.class "text-5xl text-red-500" ]
+                                [ --Icon.viewIcon Icon.playCircle
+                                  Icon.viewIcon Icon.pauseCircle
+                                ]
+                            , div
+                                []
+                                [ Icon.viewIcon Icon.forward
+                                ]
+                            , div
+                                []
+                                [ svg
+                                    [ SvgAttr.class "w-5 h-5"
+                                    , SvgAttr.fill "currentColor"
+                                    , SvgAttr.viewBox "0 0 20 20"
+                                    ]
+                                    [ Svg.path
+                                        [ SvgAttr.d "M5 4a2 2 0 0 0-2 2v6H0l4 4 4-4H5V6h7l2-2H5zm10 4h-3l4-4 4 4h-3v6a2 2 0 0 1-2 2H6l2-2h7V8z"
+                                        ]
+                                        []
+                                    ]
+                                ]
+                            ]
+                        , div
+                            [ Attr.class "flex justify-between text-sm text-gray-700"
+                            ]
+                            [ p []
+                                [ Html.text "0:42" ]
+                            , p []
+                                [ Html.text "4:20" ]
+                            ]
+                        , div
+                            []
+                            [ div
+                                [ Attr.class "h-1 bg-gray-800 rounded-full"
+                                ]
+                                [ div
+                                    [ Attr.class "w-1/5 h-1 bg-red-500 rounded-full relative"
+                                    ]
+                                    []
+                                ]
+                            ]
+                        , outlets latestAlbumOutlets "text-gray-700 text-lg md:text-base mt-4" "px-2"
+                        ]
+                    ]
                 ]
-              <|
-                releaseSelector selectors
             ]
         ]
-
-
-selectors : List ( String, ReleaseKind )
-selectors =
-    [ ( "all", All )
-    , ( "albums", Albums )
-    , ( "eps", Eps )
-    , ( "remixes", Remixes )
-    , ( "collages", Collages )
-    ]
-
-
-releaseSelector : List ( String, ReleaseKind ) -> List (Html Msg)
-releaseSelector list =
-    List.map
-        (\sel ->
-            li
-                [ Attr.class "m-2 mt-0 hover:underline hover:underline-offset-4"
-                , Aria.label "kind of release selectors"
-                , onClick (ChooseRelease (Tuple.second sel))
-                ]
-                [ a
-                    [ Attr.href <| String.append "#" (Tuple.first sel)
-                    ]
-                    [ Html.text <| Tuple.first sel ]
-                ]
-        )
-        list
-
-
-discog : Model -> Html Msg
-discog model =
-    div
-        [ Attr.class "grid grid-flow-col auto-cols-max gap-2 overflow-contain overflow-x-scroll overflow-y-hidden snap-mandatory snap-x"
-        , Aria.label "all published releases"
-        ]
-    <|
-        renderReleases <|
-            case model.showRelease of
-                All ->
-                    showReleases allReleases
-
-                Albums ->
-                    showReleases allAlbums
-
-                Eps ->
-                    showReleases allEps
-
-                Remixes ->
-                    showReleases allRemixes
-
-                Collages ->
-                    showReleases allCollages
-
-
-renderReleases : List Release -> List (Html msg)
-renderReleases data =
-    List.map
-        (\i ->
-            div
-                [ Attr.class "relative snap-always snap-center min-w-fit object-cover cursor-pointer group hover:bg-slate-900" ]
-                [ img
-                    [ Attr.class "aspect-square w-full h-auto max-w-xs lg:max-w-md group-hover:opacity-70"
-                    , Attr.src <| i.cover
-                    , Attr.alt i.name
-                    , Attr.title i.name
-                    ]
-                    []
-                , div
-                    [ Attr.class "absolute top-1/2 w-full invisible group-hover:visible text-center group-hover:text-white group-hover:text-sm group-hover:lg:text-md font-light"
-                    ]
-                    [ Html.text i.name ]
-                ]
-        )
-        data
 
 
 
@@ -520,7 +521,7 @@ about =
     section
         [ Attr.id "about", Attr.class "mx-0 md:mx-2", Aria.label "about section" ]
         [ div [ Attr.class "border-t border-slate-300 md:px-14" ]
-            [ h2 [ Attr.class "sm:font-semibold pt-4 mb-4 md:mb-0 underline text-center text-base" ]
+            [ h2 [ Attr.class "font-semibold pt-4 mb-4 md:mb-0 underline text-center text-base" ]
                 [ Html.text "about"
                 ]
             , div [ Attr.class "p-0 px-4 md:py-4 md:px-0 text-center sm:text-left text-base" ]
